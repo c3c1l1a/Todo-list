c5d047be8fbf89acf48c5f557bfe85c123c65ac2
c3c1l1a <cesswangumukima@gmail.com>
2022-06-01 22:31:43 +0800

    Add onload function

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import Model from './modules/Model.js';
 import View from './modules/View.js';
 import Controller from './modules/Controller.js';
 
-const controller = new Controller(new Model(), new View());
-controller.populateItems();
+window.onload = () => {
+	const controller = new Controller(new Model(), new View());
+	controller.populateItems();
+};
+
