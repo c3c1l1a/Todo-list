4ee1da7f7d2674ce08efa380f7a0a7acccf6bac3
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-06-09 09:15:47 +0100

    Test that works

--- testAdd.test.js
+++ testAdd.test.js
@@ -1,23 +1,11 @@
 import Model from './src/modules/Model.js';
 
-// test('properly adds Model', () => {
-//   const model = [1, 2, 3];
-//   expect(Model(model)).toEqual(model);
-//   expect(Model(model)).not.toBe(model);
-// });
-
-jest.mock('./src/modules/Model.js', () => ({
-  __esModule: true,
-  default: {
-    get: jest.fn().mockResolvedValue({ data: { name: 'Mock model' } }),
-  },
-}));
-
 describe('Model', () => {
-  test('Should return Model', async () => {
-    const result = await Model(1);
+  test('Should return Model', () => {
+    const result = new Model();
     // eslint-disable-next-line no-console
     console.log(result);
-    expect(result).toBe('Model');
+    result.updateLocalStorage([]);
+    expect(result.items).toEqual([]);
   });
 });
